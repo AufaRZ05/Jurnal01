8f402ad83c814411267300e8b85afd353475d0cf
AufaRZ01 <aufa.rzp10@gmail.com>
2023-09-28 14:38:08 +0700

    Second Commit

--- Jurnal01.java
+++ Jurnal01.java
@@ -1,5 +1,7 @@
-public class {
+public class Jurnal01 {
     public static void main(String[] args) {
-        
+        System.out.println("Nama : Aufa Riza Zaki Pradana");
+        System.out.println("NIM  : 607062330127");
+        System.out.println("Kelas: D3IF-47-01");
     }
 }
